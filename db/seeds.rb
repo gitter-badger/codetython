ae445b0b575c22e0e60d88e321e1caa1c7c49adb
Iago Effting <iago.effting@gmail.com>
2015-01-19 00:05:17 -0200

    Configurado Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new(
+  :email                 => "admin@xxxxx.xxx",
+  :password              => "123456",
+  :password_confirmation => "123456"
+)
+user.skip_confirmation!
+user.save!
